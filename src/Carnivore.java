2a555256c79c24734220a14ed9908c135d6d5463
Ludvig Kirkeby <ludvig.elias.kirkeby@gmail.com>
2024-11-21 15:44:12 +0100

    Javadoc and getFoodValue on rabbit

--- src/Carnivore.java
+++ src/Carnivore.java
@@ -2,9 +2,16 @@ import itumulator.world.World;
 
 public abstract class Carnivore extends Animal {
 
-    // Nedenstående er eks kode og ikke reelt kode
+    /**
+     * Eats an animal and reduces hunger by the animals food value (See Animal.getFoodValue())
+     * @param world World to find prey in
+     * @param target The target to eat. The target gets Deleted from world
+     */
+
     public void eat(World world, Animal target) {
-            hunger--;
+        if (hunger > 0) {
+            hunger -= target.getFoodValue();
+        }
             world.delete(target);
     }
 }
