ce7ee4396311564214cbd6f7a13fd9dfb4185392
V0idshock <vitusbrodersen@gmail.com>
2024-11-21 16:17:08 +0100

    not done reader

--- src/Reader.java
+++ src/Reader.java
@@ -1,2 +1,31 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Reader {
+    private File file;
+    private int worldSize;
+    private HashMap<String, Integer> entities;
+
+    public Reader(File file) { //constructor that has file from start
+        this();
+        this.file = file;
+    }
+    public Reader() { //empty generic instantiation constructor
+        entities = new HashMap<String, Integer>();
+        //instantiation
+    }
+    public void loadFile(File file) {this.file = file;}
+    public void readFile() {
+        entities = new HashMap<String, Integer>();
+        //Read line one
+    }
+    public int getWorldSize() {
+        return worldSize;
+    }
+
+    public HashMap getEntities() {
+        return entities;
+    }
 }
