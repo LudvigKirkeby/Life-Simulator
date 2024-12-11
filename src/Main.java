867fd874c2d57a405fa4364eb05788c019e1cce8
Ludvig Kirkeby <ludvig.elias.kirkeby@gmail.com>
2024-12-11 16:08:32 +0100

    Cleaned up Main

--- src/Main.java
+++ src/Main.java
@@ -19,35 +19,13 @@ public class Main {
 
         Reader reader = new Reader();
         //reader.loadFile("src/inputs_week-1/tf1-1.txt");
-        reader.loadFile("src/inputs_week-2/t2-4b.txt");
+        //reader.loadFile("src/inputs_week-2/t2-4b.txt");
         //reader.loadFile("src/inputs_week-3/t3-2ab.txt");
 
         reader.readFile();
 
         Program p = new Program(reader.getWorldSize(), 800, 100);
         World w = p.getWorld();
-
-/*
-Program p = new Program(20, 800, 100);
-        World w = p.getWorld();
-
-
-        for (int i = 0; i < 30; i++) {
-        placement.placeRandomly(w, new Grass());
-        }
-
-        for (int i = 0; i < 8; i++) {
-        placement.placeRandomly(w, new Bush());
-        placement.placeRandomly(w, new Wolf());
-        placement.placeRandomly(w, new Rabbit());
-        placement.placeRandomly(w, new Rabbit());
-        }
-
-Placement placement = new Placement();
-        for (int i = 0; i < 4; i++) {
-            placement.placeRandomly(w, new Bear());
-        }
- */
 
         reader.Execute(w);
         p.show();
